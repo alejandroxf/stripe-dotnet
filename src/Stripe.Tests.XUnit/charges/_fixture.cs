0af26bd4e88e9cd1541b9f601f4840c05b05343f
Remi Jannel <remi@stripe.com>
2017-08-09 19:09:03 -0400

    Properly test the extra fields for the Charge shipping hash

--- src/Stripe.Tests.XUnit/charges/_fixture.cs
+++ src/Stripe.Tests.XUnit/charges/_fixture.cs
@@ -34,6 +34,7 @@ namespace Stripe.Tests.Xunit
             };
 
             ChargeUpdateOptions.Shipping.Phone = "8675309";
+            ChargeUpdateOptions.Shipping.TrackingNumber = "56789";
 
             UpdatedCharge = service.Update(Charge.Id, ChargeUpdateOptions);
         }
